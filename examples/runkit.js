03855977542d3d27a323d0a436725a2021ee7ddb
xhayper <46065349+xhayper@users.noreply.github.com>
2023-11-17 22:47:26 +0700

    feat: update stuff

--- examples/runkit.js
+++ examples/runkit.js
@@ -1,12 +1,4 @@
-# AngpaoAPI
-
-API wrapper around Truemoney Wallet's angpao system
-
-## Usage
-
-```ts
-import { AngpaoAPI } from 'angpao-api';
+const { AngpaoAPI } = require('angpao-api');
 
 const angpao = new AngpaoAPI('089xxxxxxx');
 angpao.redeem(AngpaoAPI.extractVoucherId('https://gift.truemoney.com/campaign/?v=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'));
-```
